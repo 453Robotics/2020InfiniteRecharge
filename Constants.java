03037022692717e8ad8169ef60493df9aaabae90
Maddog4302 <48026104+Maddog4302@users.noreply.github.com>
2020-02-08 12:36:14 -0500

    Add files via upload

--- Constants.java
+++ Constants.java
@@ -1,8 +1,10 @@
-
 package frc.robot;
+
+import com.revrobotics.ColorMatch;
 
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.util.Color;
 
 public final class Constants {
     public static final class motorIDConst{
@@ -19,11 +21,17 @@ public final class Constants {
        public static final int colorWheelChooserID = 45;
        public static final int colorSensor = 60;
        public static final int canifier = 20;
-	   
+
     }
 	public static final class encoderConstants{
 	    public final static Encoder m_shooterEncoder = new Encoder(0, 1,true);
 		public final static SimpleMotorFeedforward m_shooterFeedforward = new SimpleMotorFeedforward(0.05, 0.24);
-		
-	}
-}
+
+    }
+    public static final class colorCodes{
+        private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+        private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+        private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+        private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+    }
+} 
